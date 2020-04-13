22cb0a9360353d5982cd473a2321c1e089767a25
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-04-13 23:14:50 +0300

    Add Button(LightOn ) + constraint

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        lightOn = !lightOn
+        updateUi()
+    }
+    
+    func updateUi() {
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
